d92747bc10c9b0512e1c7a16e601201442c7d8d3
Rust <you@example.com>
2019-12-30 15:31:11 +0500

    up

--- arraymapper/ArrayProcessor.php
+++ arraymapper/ArrayProcessor.php
@@ -55,7 +55,9 @@ class ArrayProcessor {
                     $c = $c->{$step}->{$id};
                 }
 
-                $c->{$fieldName} = $val;
+                if($val['id'] !== null){
+                    $c->{$fieldName} = $val;
+                }
             }
         }
 
